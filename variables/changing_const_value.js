3b027c1c372187416c39bd4f5fa423b4bab8e4bd
haider-codes23 <100827019+haider-codes23@users.noreply.github.com>
2022-03-14 16:16:57 +0500

    Update changing_const_value.js

--- variables/changing_const_value.js
+++ variables/changing_const_value.js
@@ -7,6 +7,6 @@ NAME = 'Joe';
 console.log('Good Morning, ' + NAME);
 console.log('Good Afternoon, ' + NAME);
 console.log('Good Evening, ' + NAME);
-// we get a type error due to this statement because const type variables cannot be reassigned
+// we get a type error due to the statement on line 6 because const type variables cannot be reassigned
 // instead if we used let keyword to decalre the NAME varibale this program will work. We would then
 // have three greeting statements for Victor and three greeting statements for Joe
